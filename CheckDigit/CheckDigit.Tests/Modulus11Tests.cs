9782e780a5f97d021525eff8868da96b81f397b7
Breno Martinusso <martinusso@gmail.com>
2014-04-29 11:41:09 -0300

    add check digit based on Modulus 11

---
+++ CheckDigit/CheckDigit.Tests/Modulus11Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CheckDigit;
+
+namespace CheckDigit.Tests
+{
+    [TestClass]
+    public class Modulus11Tests
+    {
+        [TestMethod]
+        public void TestShouldReturnCorrectCheckDigit()
+        {
+            string number;
+            string expectedCheckDigit;
+            string checkdigit;
+
+            number = "3214020082778300190055001000005971122014397";
+            expectedCheckDigit = "4";
+            var mod11 = new Modulus11();
+            checkdigit = mod11.GetCheckDigit(number);
+            Assert.AreEqual(expectedCheckDigit, checkdigit);
+
+            number = "3214041306259400010755001000001039113062594";
+            expectedCheckDigit = "7";
+            checkdigit = mod11.GetCheckDigit(number);
+            Assert.AreEqual(expectedCheckDigit, checkdigit);
+        }
+    }
+}
---
+++ CheckDigit/CheckDigit/Modulus11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckDigit
+{
+    public class Modulus11
+    {
+        public Modulus11() { }
+
+        public string GetCheckDigit(string number)
+        {
+            int sum = 0;
+            for (int i = number.Length - 1, multiplier = 2; i >= 0; i--)
+            {
+                sum += (int)char.GetNumericValue(number[i]) * multiplier;
+                if (++multiplier > 9) multiplier = 2;
+            }
+
+            int mod = (sum % 11);
+
+            if (mod == 0 || mod == 1) return "0";
+
+            return (11 - mod).ToString();
+        }
+    }
+}
