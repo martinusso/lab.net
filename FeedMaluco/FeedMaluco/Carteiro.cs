8d18c64fce8f7be99095ee96d4d32d996255ef9e
Breno Martinusso <martinusso@gmail.com>
2014-10-30 12:13:13 -0200

    refatoração

--- FeedMaluco/FeedMaluco/Carteiro.cs
+++ FeedMaluco/FeedMaluco/Carteiro.cs
@@ -6,32 +6,17 @@ namespace FeedMaluco
 {
     class Carteiro
     {
+        private MailMessage Mensagem;
         private Remetente Remetente;
-        private string EmailDestinatario;
-        private string Titulo;
-        private string Conteudo;
 
-        public Carteiro(Remetente remetente, string emailDestinatario, string titulo, string conteudo)
+        public Carteiro(MailMessage mensagem, Remetente remetente)
         {
+            Mensagem = mensagem;
             Remetente = remetente;
-            EmailDestinatario = emailDestinatario;
-            Titulo = titulo;
-            Conteudo = conteudo;
         }
 
         public string EnviarEmail() 
         {
-            var mail = new MailMessage();
-            mail.To.Add(EmailDestinatario);
-            mail.From = new MailAddress(Remetente.Email, Remetente.Nome, System.Text.Encoding.UTF8);
-            mail.Subject = "FeedMaluco: " + Titulo;
-            mail.SubjectEncoding = System.Text.Encoding.UTF8;
-            mail.Body = Conteudo;
-            mail.BodyEncoding = System.Text.Encoding.UTF8;
-            mail.IsBodyHtml = true;
-            mail.Priority = MailPriority.High;
-
-
             SmtpClient client = new SmtpClient(Remetente.SmtpHost, Remetente.SmtpPort);
             client.EnableSsl = Remetente.SmtpUseSSL;
             client.UseDefaultCredentials = false;
@@ -39,7 +24,7 @@ namespace FeedMaluco
 
             try
             {
-                client.Send(mail);
+                client.Send(Mensagem);
                 return "E-mail enviado com sucesso!";
             }
             catch (Exception ex)
---
+++ FeedMaluco/FeedMaluco/CriadorMensagem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace FeedMaluco
+{
+    class CriadorMensagem
+    {
+        private Remetente Remetente;
+        private string EmailDestinatario;
+        private string Titulo;
+        private string Conteudo;
+
+        public CriadorMensagem(Remetente remetente, string emailDestinatario, string titulo, string conteudo)
+        {
+            Remetente = remetente;
+            EmailDestinatario = emailDestinatario;
+            Titulo = titulo;
+            Conteudo = conteudo;
+        }
+
+        public MailMessage GetMensagem()
+        {
+            var mail = new MailMessage();
+            mail.To.Add(EmailDestinatario);
+            mail.From = new MailAddress(Remetente.Email, Remetente.Nome, System.Text.Encoding.UTF8);
+            mail.Subject = "FeedMaluco: " + Titulo;
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;
+            mail.Body = Conteudo;
+            mail.BodyEncoding = System.Text.Encoding.UTF8;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+
+            return mail;
+        }
+    }
+}
