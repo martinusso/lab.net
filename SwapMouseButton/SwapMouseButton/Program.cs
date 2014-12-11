dbb429bcce9bd245c9a5eb4d6525cbeac36d8828
Breno Martinusso <martinusso@gmail.com>
2014-12-11 13:58:12 -0200

    swapmousebutton

---
+++ SwapMouseButton/SwapMouseButton/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using System.Runtime.InteropServices;
+
+using Microsoft.Win32;
+
+namespace SwapMouseButton
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern Int32 SwapMouseButton(Int32 bSwap);
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) return;
+            switch (args[0])
+            {
+                case "left":
+                    SwapMouseButton(1); 
+                    break;
+                case "right":
+                    SwapMouseButton(1);
+                    break;
+            }
+        }
+    }
+}
