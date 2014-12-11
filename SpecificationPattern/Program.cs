f750e5a89b26f77e782a45068a8ab163b54b966c
Breno Martinusso <martinusso@gmail.com>
2014-12-11 17:28:08 -0200

    specification pattern

---
+++ SpecificationPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecificationPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mulher = new Pessoa()
+            {
+                Nome = "Leandra",
+                Sexo = Sexo.Feminino
+            };
+
+            var specification = new SpecificationMulherDisponivel();
+
+            var mulherDisponivel = specification.EstaDisponivel().Compile()(mulher);
+
+            if (mulherDisponivel)
+            {
+                Console.WriteLine("mulher está disponível");
+            }
+            else
+            {
+                Console.WriteLine("mulher NÃO está disponível");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ SpecificationPattern/SpecificationMulherDisponivel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecificationPattern
+{
+    class SpecificationMulherDisponivel
+    {
+        public Expression<Func<Pessoa, bool>> EstaDisponivel()
+        {
+            return pessoa => pessoa.Sexo == Sexo.Feminino;
+        }
+    }
+}
